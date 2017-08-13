c2eb2e1e898345649d4faed8bf25d3000b7d4798
wangyongsd2011@163.com <wangyongsd2011@163.com>
2017-08-13 21:50:53 +0800

    order.js

--- Source/shopSystem/src/main/webapp/WEB-INF/views/pages/js/tOrder.js
+++ Source/shopSystem/src/main/webapp/WEB-INF/views/pages/js/tOrder.js
@@ -4,46 +4,41 @@
 	var tableName       = 'Order';
 	var initQueryFunction = 'findByDelFlag';
 	
+	//表中初始化
 	function init(){
 		hostCrud.hostInit(respositoryName,idName,initQueryFunction);
 	}
 	
-	
+	//模态框显示
 	function showModelWindow() {
 		hostCrud.showModelWindow(tableName);
 		
 	};
 
+	//关闭模态框
 	function closeModelWindow() {
 		
 		hostCrud.closeModelWindow();
 		init();
 	}
-		
-
+	//点击新建
 	function newObj() {
 		showModelWindow();
 	};
 
+	//保存数据
 	function saveObj() {
 		var obj = $('#fm').serializeObject();
 		hostCrud.saveObj(obj);
 	};
-
 	
-	
-	function rCreate(createObj) {
-		hostCrud.rCreate(createObj);
-	};
-
+	//编辑对象
 	function editObj() {
-		
 		var obj = $('#dg').datagrid('getSelected');
 		hostCrud.editObj(obj,tableName);
 	};
 
-	
-
+	//删除
 	function removeObj() {
 		///物理删除代码
 		var row = $('#dg').datagrid('getSelected');
